c7e9958d2c668f755971c95cfc34f63cc3d09728
Nahdaa <jnahdaa@gmail.com>
2023-03-29 23:27:28 +0100

    Added a 'Play Again' option for the user.

--- gtnScript.py
+++ gtnScript.py
@@ -9,15 +9,23 @@ import random
 def guessNum():
     randInt = random.randint(0,9)
     guess = 0
+    playAgain = 0
 
-    while guess!=randInt:
-        guess = int(input("Guess a number between 1 and 10 inclusive: "))
-        if guess>randInt:
-            print("Too high! Go lower!")
+    while playAgain != "N":
+        while guess!=randInt:
+            guess = int(input("Guess a number between 1 and 10 inclusive: "))
+            if guess>randInt:
+                print("Too high! Go lower!\n")
 
-        elif guess<randInt:
-            print("Too low! Go higher!")
+            elif guess<randInt:
+                print("Too low! Go higher!\n")
 
-    print("Congratulations! You guessed correctly!")
+        print(f'Congratulations! {randInt} was the correct answer!\n')
+        playAgain = input("Would you like to play again? (Y/N) \n")
+        if playAgain == "Y":
+            guess = 0
+            randInt = random.randint(0, 9)
+
+    print("Thanks for playing!")
 
 guessNum()
